5e14eef67375f0b2be0dc9ab56d8be3b67fab54a
toqvist <tobias-ahlqvist@outlook.com>
2022-01-31 11:27:02 +0100

    Added comments.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Skriv all kod här
-//KEY: 988b6ed27d7e444cabd31e2df91bdfc7
+//API-key from weatherbit.io
 const KEY = '988b6ed27d7e444cabd31e2df91bdfc7'
 
+//Get input form and button elements from the DOM
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 
-btn.innerText = 'Search'
+//When button is clicked, call the search function
 btn.addEventListener('click', search)
 
-
-//Select current-weather display elements
+//Select all display elements so that we can modify their content later
 const currentDescription = document.getElementById('current-description');
 const currentTemp = document.getElementById('current-temp');
 const currentWind = document.getElementById('current-wind');
@@ -21,9 +21,13 @@ const errorMessage = document.getElementById('error-message');
 const forecastElements = document.getElementsByClassName('forecast');
 
 function search () {
+    //Remove error message (if it exists)
     clearDisplay();
+
+    //Get input from form
     let city = input.value;
-    
+
+    //Make API calls for current weather and weather forecast
     getCurrentWeather(city);
     getForecast(city);
 }
@@ -34,7 +38,7 @@ function getCurrentWeather (city) {
 
     fetch(url).then(
         function (response) {
-
+            //If data is successful, return the data as a JSON
             if(response.status>=200 && response.status<300){
                 return response.json();
             }
@@ -61,6 +65,7 @@ function getForecast (city) {
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${KEY}&lang=sv`
 
     fetch(url).then(
+        //If data is successful, return the data as a JSON
         function (response) {
 
             if(response.status>=200 && response.status<300){
@@ -87,6 +92,7 @@ function getForecast (city) {
 
 function displayCurrentWeather(currentWeather) {
     
+    //Get data from JSON file and modify display element contents to match the JSON data. 
     currentDescription.innerText = `${currentWeather.data[0].weather.description}`
     currentTemp.innerText = `Temperatur: ${Math.round(currentWeather.data[0].temp)}`;
     currentWind.innerText = `Vindhastighet: ${Math.round(currentWeather.data[0].wind_spd)};`
@@ -99,13 +105,18 @@ function displayCurrentWeather(currentWeather) {
 
 function displayForecast(forecast) {
     
-    //Dubbelkolla så att 0 inte är idag
+    //Loop through an array of forecast-div-card-elements
     for (let i=0;i<5;i++) {
       
+        //Get the display elements of the current forecast card
         const imgE = forecastElements[i].querySelector("img");
         const descE = forecastElements[i].querySelector(".description")
         const tempE = forecastElements[i].querySelector(".temperature")
         
+
+        //Get data from JSON and modify the display elements of each forecast card. 
+        //We use i+1 because we are not interested in the forecast for today, 
+        //index 0 is today's forecast.
         const icon = forecast.data[i+1].weather.icon;
         imgE.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`
 
@@ -117,7 +128,6 @@ function displayForecast(forecast) {
 
 function displayError (){
     errorMessage.innerText = "City not found."
-
 }
 
 function clearDisplay () {
